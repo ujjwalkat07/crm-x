99010d78f52102338f783bc089d15d288bd3a614
ujjwalkat07 <ukcode07@gmail.com>
2026-06-29 14:13:23 +0530

    feat: initialize server entry point and start application listener

--- apps/crm-server/src/server.ts
+++ apps/crm-server/src/server.ts
@@ -2,8 +2,12 @@ import app from "./app";
 import { config } from "./config/env-config/config";
 
 const PORT = config.PORT || 3001;
+const startServer = () => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+}
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+startServer()
+
 
